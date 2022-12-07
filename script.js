b875cf6a8f69ba80c435cda3f9dbd2a1c459b6fd
aoi okamura <aogangcun@gmail.com>
2022-12-07 19:41:35 +0900

    ⚡️ ローカルストレージに保存

--- script.js
+++ script.js
@@ -2,13 +2,26 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+  todos.forEach(todo => {
+    add(todo);
+  })
+}
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   add();
 });
 
-function add() {
+function add(todo) {
   let todoText = input.value;
+
+  if (todo) {
+  todoText = todo;
+  }
+
   if (todoText) {
     const li = document.createElement("li");
     li.innerText = todoText;
@@ -25,5 +38,5 @@ function saveData () {
   lists.forEach(list => {
     todos.push(list.innerText);
   });
-  
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
